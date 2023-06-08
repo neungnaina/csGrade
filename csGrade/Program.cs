2bf49e3b0c64d8be30484487caff30f8509367b8
neungnaina <naiasdf@Gmail.com>
2023-06-08 13:36:04 +0700

    Add project files.

---
+++ csGrade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csGrade
+{
+    internal class Program
+    {
+        /*
+         * int Num_Std = 0;
+        string[] Name_std = new string[0];
+        int[] Score_Mid = new int[];
+        int[] Score_Final = new int[];
+        int[] Score_total = new int[];
+        float[] Grade = new float[];
+        */
+        static void Main(string[] args)
+        {
+
+            int Num_Std = 0;
+            string[] Name_std = new string[Num_Std];
+            int[] Score_Mid = new int[Num_Std];
+            int[] Score_Final = new int[Num_Std];
+            int[] Score_total = new int[Num_Std];
+            float[] Grade = new float[Num_Std];
+            if (Score_total[0] <= 49)
+            {
+                float[]
+            }
+            Console.ReadKey();
+        }
+        
+         
+        
+    }
+}
